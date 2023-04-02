38a6c0889909be53f23e897f919950539ba674c9
Tyler Fritz <tfritz@torticity.com>
2023-04-02 18:15:56 -0400

    .

--- composables/useIssueReenderedFieldsQuery.ts
+++ composables/useIssueReenderedFieldsQuery.ts
@@ -1,10 +1,3 @@
 export function useIssueRenderedFieldsQuery(issueKeyOrId: string | number) {
-  const route = useRoute()
-  const query = route.query
-
-  const firstKey = Object.keys(query)?.[0]
-
-  return useFetch(
-    `/api/jira/rest/api/3/issue/${issueKeyOrId}?expand=description?${firstKey},renderedFields`,
-  )
+  return useFetch(`/api/jira/rest/api/3/issue/${issueKeyOrId}?expand=description,renderedFields`)
 }
