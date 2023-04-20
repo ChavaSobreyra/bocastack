eb34063e181fa6f517a26f4134fc90bab20bc802
ChavaSobreyra <chava.sobreyra@gmail.com>
2023-04-20 10:17:12 -0400

    .

--- components/IssuesPanel.vue
+++ components/IssuesPanel.vue
@@ -6,11 +6,11 @@
       </button>
     </div>
 
-    <p class="mt-1.5 text-xl text-gray-900">
+    <div class="mt-1.5 text-xl text-gray-900">
       <span v-if="data.fields.flagged" class="mr-2">🛑</span>
       <span v-if="data.fields.status.name === 'UAT'" class="mr-2">👀</span>
       <span class="font-semibold text-gray-600">{{ data.fields.summary }}</span>
-    </p>
+    </div>
     <div class="flex items-center justify-self-start border-b-2 border-gray-200 p-6">
       <img
         v-if="data.fields.assignee"
@@ -19,19 +19,19 @@
         alt=""
       />
       <span class="pl-4 font-semibold text-gray-600">Points:</span>
-      <p class="rounded-full bg-gray-100 px-1 text-center font-semibold text-gray-800">
+      <div class="rounded-full bg-gray-100 px-1 text-center font-semibold text-gray-800">
         {{ data.fields.customfield_10028 ? data.fields.customfield_10028 : '-' }}
-      </p>
+      </div>
       <span class="pl-2 pr-2 font-semibold text-gray-600">Epic:</span>
-      <p
-        class="rounded-full p-2 text-center font-semibold text-white"
+      <div
+        class="rounded-full p-2 text-center text-sm font-semibold text-white"
         :class="{
           'bg-blue-500': data.fields.parent.fields.summary !== 'Bug',
           'bg-red-500': data.fields.parent.fields.summary === 'Bug',
         }"
       >
         {{ data.fields.parent.fields.summary }}
-      </p>
+      </div>
     </div>
     <div
       class="break-words"
@@ -80,5 +80,9 @@ function replaceAttachments(text: string, attachments: any[]) {
   border-left: 1px solid #ccc;
   padding: 20px;
   overflow-y: scroll;
+}
+
+.side-panel p {
+  margin-top: 1em;
 }
 </style>
