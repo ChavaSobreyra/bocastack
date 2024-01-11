154b884dcc6c64e42cd8db509551c5427f0d6cd8
ChavaSobreyra <chava.sobreyra@gmail.com>
2024-01-11 15:21:11 -0500

    🐢

--- components/Issues.vue
+++ components/Issues.vue
@@ -1,7 +1,7 @@
 <template>
   <ul role="list" class="divide-y divide-gray-200">
     <li
-      v-for="(issue, i) of issues"
+      v-for="issue of issues"
       :key="issue.id"
       class="grid select-none grid-flow-col py-3 px-4 hover:bg-gray-100"
       :class="{
