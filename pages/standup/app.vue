f536c5fb603800507e806704a8c4c22a57318d29
ChavaSobreyra <chava.sobreyra@gmail.com>
2024-09-19 12:07:58 -0400

    🍩

--- pages/standup/app.vue
+++ pages/standup/app.vue
@@ -12,11 +12,11 @@
     <div class="container relative z-50 mx-auto max-w-3xl justify-self-center pb-16">
       <Nav />
       <section class="pt-8">
-        <div class="overflow-hidden rounded bg-white px-8 py-11">
-          <div class="grid grid-cols-2 items-center px-1 pb-8 font-bold text-gray-900">
-            <span class="justify-self-start text-lg">Recently completed</span>
-            <span class="justify-self-end text-4xl">🎉 🥳</span>
-          </div>
+        <div class="grid grid-cols-2 items-center px-1 pb-2 font-bold text-gray-900">
+          <span class="justify-self-start text-lg">Recently completed</span>
+          <span class="justify-self-end text-3xl">🎉 🥳</span>
+        </div>
+        <div class="overflow-hidden rounded bg-white p-6">
           <Issues
             v-if="activeSprintId"
             :active-sprint-id="activeSprintId"
@@ -33,7 +33,7 @@
           <span class="justify-self-start text-lg">Work in progress</span>
           <span class="justify-self-end text-3xl">🚧</span>
         </div>
-        <div class="overflow-hidden rounded bg-white px-6 py-8">
+        <div class="overflow-hidden rounded bg-white p-6">
           <Issues
             v-if="activeSprintId"
             :active-sprint-id="activeSprintId"
@@ -49,7 +49,7 @@
           <span class="justify-self-start text-lg">Upcoming</span>
           <span class="justify-self-end text-3xl">⏰</span>
         </div>
-        <div class="overflow-hidden rounded bg-white px-6 py-8">
+        <div class="overflow-hidden rounded bg-white p-6">
           <Issues
             v-if="activeSprintId"
             :active-sprint-id="activeSprintId"
